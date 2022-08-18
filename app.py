2317dad4539238e72d8983887f3f6c4f021473ce
Naman Mehra <naman.mehra,3000@gmail.com>
2022-08-19 02:27:38 +0530

    Added ToLang

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from pydantic import BaseModel
 
 class translateClass(BaseModel):
     sentence:str
+    toLang:str
 
 
 app = FastAPI()
 
 @app.post("/home/")
 async def getRecommendation(translation:translateClass):
-    translator = Translator(from_lang="en",to_lang="hi")
+    translator = Translator(from_lang="en",to_lang=translation.toLang)
 
     translation=translator.translate(translation.sentence)
         
