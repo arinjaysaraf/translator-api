214d4492a4903b05cc62668ee51027b89ee35265
Arinjay Saraf <arinjaysaraf@gmail.com>
2022-08-20 00:55:50 +0530

    edited app

--- app.py
+++ app.py
@@ -1,20 +1,63 @@
-from translate import  Translator
+from translate import Translator
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+import re
+alphabets= "([A-Za-z])"
+prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+websites = "[.](com|net|org|io|gov)"
+digits = "([0-9])"
+
+def split_into_sentences(text):
+    print("text",text)
+    text = " " + text + "  "
+    text = text.replace("\n"," ")
+    text = re.sub(prefixes,"\\1<prd>",text)
+    text = re.sub(websites,"<prd>\\1",text)
+    text = re.sub(digits + "[.]" + digits,"\\1<prd>\\2",text)
+    if "..." in text: text = text.replace("...","<prd><prd><prd>")
+    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
+    text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
+    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
+    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
+    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
+    text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
+    if "”" in text: text = text.replace(".”","”.")
+    if "\"" in text: text = text.replace(".\"","\".")
+    if "!" in text: text = text.replace("!\"","\"!")
+    if "?" in text: text = text.replace("?\"","\"?")
+    text = text.replace(".",".<stop>")
+    text = text.replace("?","?<stop>")
+    text = text.replace("!","!<stop>")
+    text = text.replace("<prd>",".")
+    sentences = text.split("<stop>")
+    # sentences = sentences[:-1]
+    sentences = [s.strip() for s in sentences]
+    return sentences
 
 class translateClass(BaseModel):
     sentence:str
     toLang:str
+    fromLang:str
 
 
 app = FastAPI()
 
 @app.post("/home/")
 async def getRecommendation(translation:translateClass):
-    translator = Translator(from_lang="en",to_lang=translation.toLang)
-
-    translation=translator.translate(translation.sentence)
-        
+    translator = Translator(from_lang=translation.fromLang,to_lang=translation.toLang)
+    print(translation.sentence)
+    sentence_split = split_into_sentences(translation.sentence)
+    finalSentence=""
+    print(sentence_split)
+    for i in sentence_split:
+        translation=translator.translate(i)
+        finalSentence=finalSentence+translation+" "
     return [{
-        "Translation":translation
+        "Translation":finalSentence
     }]
